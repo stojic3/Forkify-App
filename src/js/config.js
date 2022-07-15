12c6e77ed5566d045776c4dcdd5fa87e7b0a9441
unknown <olgica12stojic@gmail.com>
2022-07-15 14:43:56 +0200

    full project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1fcc1439-20f7-4082-b82b-cb9f54f7f085';
+export const MODAL_CLOSE_SEC = 2.5;
